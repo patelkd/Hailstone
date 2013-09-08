5ecaf3efeed68cec3b19baab773f41e1a6fad7b9
Karishma Patel <karishma1126@yahoo.com>
2013-09-08 10:30:53 -0600

    Created Hailstone method

--- HailstoneProblem.java
+++ HailstoneProblem.java
@@ -2,4 +2,24 @@ public class HailstoneProblem {
 	public static void main(String[] args) {
 		
 	}
+
+	public static int Hailstone(int number) {
+		while (number>0 && number!=1) {
+ 			while (number % 2 == 0 && number != 1) {
+        		number = number / 2;
+         		System.out.println(number);
+ 	   		 }
+
+  			while (number % 2 != 0 && number !=1) {
+         		number = ((number * 3) + 1);
+         		System.out.println(number);
+ 	    	}
+    	}
+
+		if (number == 1) {
+  			System.out.println("Stop calculation");
+		}
+		return number;
+	}
+
 }
